c1e9ee3e36e4fc95c409745cf4c853656d404893
foxching <rechielagria@gmail.com>
2021-03-03 18:32:34 +0800

    project setup model,routes, installation of packages in server ; install of react in client,

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+
+//body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/api/auth', require('./routes/api/auth'));
+
+
+
+//mongodb
+const config = require('config')
+const db = config.get('mongoURI')
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, })
+    .then(() => console.log('Database connected'))
+    .catch(error => console.log(error))
+
+
+
+//port
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`));
