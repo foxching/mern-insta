6e732e140d9d2642e20b7a4706b74503345160f8
foxching <rechielagria@gmail.com>
2021-03-09 20:01:00 +0800

    Creating Signin and Signup page UI in frontend

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom"
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="mycard">
+      <div className="card card-login input-field">
+        <h2>Instagram</h2>
+        <input type="email" name="email" placeholder="Email" />
+        <input type="password" name="password" placeholder="Password" />
+        <button class="btn waves-effect waves-light blue lighten-2" type="submit" >
+          Login
+        </button>
+        <h5>
+          <Link to="/signup">Dont have an account ?</Link>
+        </h5>
+        <h6>
+          <Link to="/reset">Forgot password ?</Link>
+        </h6>
+      </div>
+    </div>
+  )
 };
 
 export default Login;
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom"
+
+
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <div className="mycard">
+      <div className="card card-login input-field">
+        <h2>Instagram</h2>
+        <input type="text" name="name" placeholder="Username" />
+        <input type="email" name="email" placeholder="Email" />
+        <input type="text" name="password" placeholder="Password" />
+        <button class="btn waves-effect waves-light blue lighten-2" type="submit" >
+          Signup
+        </button>
+        <h5>
+          <Link to="/signin">Already have an account ?</Link>
+        </h5>
+      </div>
+    </div>
+  )
 };
 
 export default Signup;
