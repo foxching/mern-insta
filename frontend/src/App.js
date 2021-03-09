c1ce52b398c707c931da74c215f116ec54a73c33
foxching <rechielagria@gmail.com>
2021-03-09 09:51:52 +0800

    add react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,22 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import "./App.css";
 
 const App = () => {
-    return (
-        <div>Welcome</div>
-    )
-}
-export default App
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/signin" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </BrowserRouter>
+    </>
+  );
+};
+export default App;
