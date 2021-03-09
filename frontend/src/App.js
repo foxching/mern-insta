687bdf1d41e12739426b222af7e906208bcfc870
foxching <rechielagria@gmail.com>
2021-03-09 20:23:13 +0800

    Creating Profile page UI in react js |

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Profile from "./pages/Profile"
 import "./App.css";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route path="/signin" component={Login} />
           <Route path="/signup" component={Signup} />
+          <Route path="/profile" component={Profile} />
         </Switch>
       </BrowserRouter>
     </>
