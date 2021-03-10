9b74e3cbe3d43f39e96406849d22aa8dcb876311
foxching <rechielagria@gmail.com>
2021-03-10 10:35:27 +0800

    Creating Profile page UI part 2

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,32 +1,60 @@
 const Profile = () => {
-    return (
-        <div style={{ maxWidth: "550px", margin: "0px auto" }}>
-            <div style={{
-                margin: "18px 0px",
-                borderBottom: "1px solid grey"
-            }} >
-                <div style={{
-                    display: "flex",
-                    justifyContent: "space-around",
-                }}>
-                    <div>
-                        <img style={{ width: "160px", height: "160px", borderRadius: "80px" }}
-                            alt=""
-                            src="https://images.unsplash.com/photo-1569124589354-615739ae007b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
-                    </div>
-                    <div>
-                        <h4>Rechie</h4>
-                        <div style={{ display: "flex", justifyContent: "space-between", width: "108%" }}>
-                            <h6> 10posts</h6>
-                            <h6>10 followers</h6>
-                            <h6>5 following</h6>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div style={{ maxWidth: "550px", margin: "0px auto" }}>
+      <div
+        style={{
+          margin: "18px 0px",
+          borderBottom: "1px solid grey"
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around"
+          }}
+        >
+          <div>
+            <img
+              style={{ width: "160px", height: "160px", borderRadius: "80px" }}
+              alt=""
+              src="https://images.unsplash.com/photo-1569124589354-615739ae007b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+            />
+          </div>
+          <div>
+            <h4>Rechie</h4>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "108%"
+              }}
+            >
+              <h6> 10posts</h6>
+              <h6>10 followers</h6>
+              <h6>5 following</h6>
             </div>
-
+          </div>
         </div>
-    );
-}
+      </div>
+      <div className="gallery">
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1506905925346-21bda4d32df4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="mountain"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1543862475-eb136770ae9b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80"
+          alt="mountain"
+        />
+        <img
+          className="item"
+          src="https://images.unsplash.com/photo-1457195740896-7f345efef228?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt="mountain"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
