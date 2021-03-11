eda5e499c868a89a8ea84f931d632247e528ccf9
foxching <rechielagria@gmail.com>
2021-03-11 20:09:45 +0800

    install redux, setup auth reducers,actions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,27 @@
-import Navbar from "./components/Navbar";
+import {useEffect} from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
 import CreatePost from "./pages/CreatePost";
+
+import store from "./redux/store";
+import { loadUser } from './redux/actions/authActions'
+
 import "./App.css";
 
 const App = () => {
+  
+  useEffect(() => {
+    store.dispatch(loadUser())
+  },[])
+  
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
         <Navbar />
         <Switch>
@@ -20,7 +32,7 @@ const App = () => {
           <Route path="/create" component={CreatePost} />
         </Switch>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 };
 export default App;
--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,13 +1,18 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
   return (
     <div className="mycard">
       <div className="card card-login input-field">
         <h2>Instagram</h2>
-        <input type="text" name="name" placeholder="Username" />
-        <input type="email" name="email" placeholder="Email" />
-        <input type="password" name="password" placeholder="Password" />
+        <input type="text" name="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="Username" />
+        <input type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
         <button
           class="btn waves-effect waves-light blue darken-1"
           type="submit"
