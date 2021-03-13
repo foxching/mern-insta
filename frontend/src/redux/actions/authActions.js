cb9f6fc8802b50c7aff9c823d578cbbe5da2e30a
foxching <rechielagria@gmail.com>
2021-03-13 18:56:48 +0800

    fixed line identation; delete unwanted file

--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -28,7 +28,6 @@ export const loadUser = () => (dispatch, getState) => {
         }   
         )
         .catch(err => {
-            
             dispatch(returnErrors(err.response.data, err.response.status));
             dispatch({
                 type: AUTH_ERROR
