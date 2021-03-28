cdf3f83fa2b8490a3340d94eedc8209cd49c4431
foxching <rechielagria@gmail.com>
2021-03-28 09:47:21 +0800

    connect redux action in updating profile photo in client

--- frontend/src/components/Profile/ProfileInfo.js
+++ frontend/src/components/Profile/ProfileInfo.js
@@ -1,86 +1,100 @@
-import React, {useState} from "react";
-import {useDispatch} from "react-redux"
-import {followUser, unFollowUser} from "../../redux/actions/userActions"
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { followUser, unFollowUser } from "../../redux/actions/userActions";
 import ProfilePhoto from "./ProfilePhoto";
 
-const ProfileInfo = ({name, pic, userId, authUserId,  loading, followers,following,  userPosts}) => {
-  const dispatch = useDispatch()
-
-
+const ProfileInfo = ({
+  name,
+  pic,
+  userId,
+  authUserId,
+  loading,
+  followers,
+  following,
+  userPosts
+}) => {
+  const dispatch = useDispatch();
 
   //get the liked scream
-	let likedPost = () => {
-		if (followers && followers.find((followerId) => followerId === authUserId)) return true;
-		else return false;
-	};
+  let likedPost = () => {
+    if (followers && followers.find(followerId => followerId === authUserId))
+      return true;
+    else return false;
+  };
 
   //like button component
-	const likeButton = authUserId === userId ? (
-		<button 
-        style={{marginTop:"10px", marginLeft:"15px"}}
-        className="btn waves-effect waves-light #efebe9 brown darken-5 btn-small" 
-    >
+  const likeButton =
+    authUserId === userId ? (
+      <button
+        style={{ marginTop: "10px", marginLeft: "15px" }}
+        className="btn waves-effect waves-light #efebe9 brown darken-5 btn-small"
+      >
         Edit
-    </button> 
-	) : likedPost() ? (
-    <button 
-      style={{marginTop:"10px", marginLeft:"15px"}}
-      className="btn waves-effect waves-light  red darken-1 btn-small"
-      onClick={() => dispatch(unFollowUser(userId))}
-    >
-    UnFollow
-    </button>
-	) : (
-    <button 
-    style={{marginTop:"10px", marginLeft:"15px"}}
-    className="btn waves-effect waves-light #64b5f6 blue darken-1 btn-small"
-    onClick={() => dispatch(followUser(userId))}
-  >
-    Follow
-  </button>
-	);
-
- 
+      </button>
+    ) : likedPost() ? (
+      <button
+        style={{ marginTop: "10px", marginLeft: "15px" }}
+        className="btn waves-effect waves-light  red darken-1 btn-small"
+        onClick={() => dispatch(unFollowUser(userId))}
+      >
+        UnFollow
+      </button>
+    ) : (
+      <button
+        style={{ marginTop: "10px", marginLeft: "15px" }}
+        className="btn waves-effect waves-light #64b5f6 blue darken-1 btn-small"
+        onClick={() => dispatch(followUser(userId))}
+      >
+        Follow
+      </button>
+    );
 
   return (
-        <div
+    <div
+      style={{
+        margin: "18px 0px",
+        borderBottom: "1px solid grey"
+      }}
+    >
+      <div
         style={{
-          margin: "18px 0px",
-          borderBottom: "1px solid grey"
+          display: "flex",
+          justifyContent: "space-around"
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around"
-          }}
-        >
-          <ProfilePhoto pic={pic} />
-          <div>
-            <div style={{display:"flex", alignItems:"center", justifyContent:"flex-start"}}>
-              {loading  ? "Loading...." : 
+        <ProfilePhoto pic={pic} loading={loading} />
+        <div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "flex-start"
+            }}
+          >
+            {loading ? (
+              "Loading...."
+            ) : (
               <React.Fragment>
-              <h4>{ name}</h4>
-              {likeButton}
+                <h4>{name}</h4>
+                {likeButton}
               </React.Fragment>
-              }
-            </div>
-           
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                width: "108%"
-              }}
-            >
-              <h6>{userPosts && userPosts.length} posts</h6>
-              <h6>{followers && followers.length} followers</h6> 
-              <h6>{following && following.length} following</h6> 
-            </div>
-           
+            )}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "108%"
+            }}
+          >
+            <h6>{userPosts && userPosts.length} posts</h6>
+            <h6>{followers && followers.length} followers</h6>
+            <h6>{following && following.length} following</h6>
           </div>
         </div>
-      </div> 
-    )
-}
-export default ProfileInfo
+      </div>
+    </div>
+  );
+};
+export default ProfileInfo;
--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -1,49 +1,70 @@
 import {
-  PROFILE_LOADING, PROFILE_LOADED, FOLLOW_USER, UNFOLLOW_USER,UPDATE_PROFILE_PHOTO
-} from '../constants/types';
-
+  LOGOUT_SUCCESS,
+  PROFILE_LOADING,
+  PROFILE_LOADED,
+  FOLLOW_USER,
+  UNFOLLOW_USER,
+  UPDATE_PROFILE_PHOTO
+} from "../constants/types";
 
 const initialState = {
-    profile: {},
-    isLoading:false,
-    userPosts:[]
+  profile: {},
+  isLoading: false,
+  userPosts: []
 };
 
 const userReducer = (state = initialState, action) => {
-    switch (action.type) {
-       case PROFILE_LOADING:
-           return {
-               ...state,
-               isLoading:true
-           }
-        case PROFILE_LOADED:
-            return {
-                ...state,
-                isLoading:false,
-                profile:action.payload.user,
-                userPosts:action.payload.posts
-            }
-        case FOLLOW_USER:
-            return {
-                ...state,
-               profile:{
-                   ...state.profile,
-                   followers:[...state.profile.followers, action.payload.userId] 
-               }
-            }
-        case UNFOLLOW_USER:
-            return {
-                ...state,
-                profile:{
-                    ...state.profile,
-                    followers:[...state.profile.followers.filter(id => id !== action.payload.userId )]
-                }
-            }
-        case UPDATE_PROFILE_PHOTO:
-            return state
-        default:
-            return state;
-    }
-}
+  switch (action.type) {
+    case PROFILE_LOADING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case PROFILE_LOADED:
+      return {
+        ...state,
+        isLoading: false,
+        profile: action.payload.user,
+        userPosts: action.payload.posts
+      };
+    case FOLLOW_USER:
+      return {
+        ...state,
+        profile: {
+          ...state.profile,
+          followers: [...state.profile.followers, action.payload.userId]
+        }
+      };
+    case UNFOLLOW_USER:
+      return {
+        ...state,
+        profile: {
+          ...state.profile,
+          followers: [
+            ...state.profile.followers.filter(
+              id => id !== action.payload.userId
+            )
+          ]
+        }
+      };
+    case UPDATE_PROFILE_PHOTO:
+      return {
+        ...state,
+        profile: {
+          ...state.profile,
+          pic: action.payload.pic
+        }
+      };
+    case LOGOUT_SUCCESS:
+      return {
+        ...state,
+        profile: {},
+        isLoading: false,
+        userPosts: []
+      };
+    default:
+      return state;
+  }
+};
 
-export default userReducer
+export default userReducer;
