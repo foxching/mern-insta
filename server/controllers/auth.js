1b8780320e709e301fedd8175cf9775ae203cf60
foxching <rechielagria@gmail.com>
2021-04-03 18:07:10 +0800

    configure sending email message on user register controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,95 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const config = require("config");
+const { JWT_SECRET, EMAIL, PASSWORD } = require("../config/keys");
 const User = require("../model/User");
+const nodemailer = require("nodemailer");
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: EMAIL,
+    pass: PASSWORD
+  }
+});
+
+/**
+ * @route   POST api/auth/register
+ * @desc    Register User
+ */
+exports.register = (req, res) => {
+  const { name, email, password, pic } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(422).json({ msg: "All fields are required" });
+  }
+
+  User.findOne({
+    $or: [
+      {
+        email: email
+      },
+      {
+        name: name
+      }
+    ]
+  })
+    .then(user => {
+      if (user)
+        return res
+          .status(400)
+          .json({ msg: "Email or username already exists" });
+      const newUser = new User({
+        name,
+        email,
+        password,
+        pic
+      });
+
+      //create salt
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw Error();
+          newUser.password = hash;
+          newUser.save().then(user => {
+            let mailOptions = {
+              from: "InstaClone",
+              to: user.email,
+              subject: "Welcome to InstaClone",
+              text: "Hi" + "" + user.name + " welcome to Insta Clone "
+            };
+
+            transporter.sendMail(mailOptions, (err, info) => {
+              if (err) return res.status(500).json({ err: err });
+              jwt.sign(
+                { id: user.id },
+                JWT_SECRET,
+                { expiresIn: 3600 },
+                (err, token) => {
+                  if (err) throw err;
+
+                  res.status(200).json({
+                    msg: "Successfully signup",
+                    token,
+                    user: {
+                      _id: user._id,
+                      name: user.name,
+                      email: user.email,
+                      pic: user.pic,
+                      followers: user.followers,
+                      following: user.following
+                    }
+                  });
+                }
+              );
+            });
+          });
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 /**
  * @route   POST api/auth/login
@@ -24,7 +112,7 @@ exports.login = (req, res) => {
       if (!isMatch) return res.status(400).json({ msg: "Wrong credentials" });
       jwt.sign(
         { id: user.id, name: user.name },
-        config.get("jwtSecret"),
+        JWT_SECRET,
         { expiresIn: 3600 },
         (err, token) => {
           if (err) throw err;
@@ -42,59 +130,6 @@ exports.login = (req, res) => {
           });
         }
       );
-    });
-  });
-};
-
-/**
- * @route   POST api/auth/register
- * @desc    Register User
- */
-exports.register = (req, res) => {
-  const { name, email, password, pic } = req.body;
-  console.log(req.body);
-
-  if (!name || !email || !password) {
-    return res.status(422).json({ msg: "All fields are required" });
-  }
-
-  User.findOne({ email }).then(user => {
-    if (user) return res.status(400).json({ msg: "User already exists" });
-    const newUser = new User({
-      name,
-      email,
-      password,
-      pic
-    });
-
-    //create salt
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw Error();
-        newUser.password = hash;
-        newUser.save().then(user => {
-          jwt.sign(
-            { id: user.id },
-            config.get("jwtSecret"),
-            { expiresIn: 3600 },
-            (err, token) => {
-              if (err) throw err;
-              res.status(200).json({
-                msg: "Successfully signup",
-                token,
-                user: {
-                  _id: user._id,
-                  name: user.name,
-                  email: user.email,
-                  pic: user.pic,
-                  followers: user.followers,
-                  following: user.following
-                }
-              });
-            }
-          );
-        });
-      });
     });
   });
 };
